bff5883577328f0c839db7bfcdcd4d5b366f0b67
GABrambila <brambila.gabriel26@gmail.com>
2024-04-25 20:44:15 -0300

    stg2Galli

---
+++ Enquetes/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <div>
+                    <a>Lista Enquetes</a>
+                    <a>Sobre</a>
+                </div>
+
+                <div>
+                    <a>Adicionar nova Enquete</a>
+                    <a>Login</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+
+
+export default Header;
